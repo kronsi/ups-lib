ed32b741b29bfc1f2fc90adc061e0e283951ba3f
Sven Kronshage <sven@kronsi.com>
2024-07-23 14:37:58 +0200

    major update to use oauth

--- example/index.js
+++ example/index.js
@@ -3,151 +3,24 @@ var util = require('util');
 var fs = require('fs');
 
 
-var ups = new upsAPI({
-  environment: 'test', // production or test
-  debug: true,
-  debugOutput: "xml",
-  AccessLicenseNumber: 'Add License Key Here',
-  UserId: 'Add User Id Here',
-  Password: 'Add Password Here',
-  ShipperNumber: "Add your Shipper Nr here",  
-  imperial: false // set to false for metric
+const ups = new upsAPI({
+    environment: 'test', // production or test
+    debug: false,
+    clientId: "your client id",
+    clientSecret: "your client secret",
+    AccessLicenseNumber: '',
+    shipperNumber: "",  
+    imperial: false // set to false for metric
 });
 
-
-/**
- * Ship
- */
-
- ups.ship({
-    Request: {      
-      TransactionReference: {
-          CustomerContext: ""
-      },      
-      RequestAction: "ShipConfirm",
-      RequestOption: "nonvalidate",
-    },    
-    Shipment: {        
-        Description: "My Shipment",
-        Shipper: {
-            Name: "",
-            AttentionName: "",
-            ShipperNumber: ups.options.ShipperNumber,
-            PhoneNumber: "",            
-            
-            Address: {
-                AddressLine: "",                
-                City: "",                
-                PostalCode: "",
-                CountryCode: ""
-            }
-        },
-        ShipTo: {
-            Name: "",
-            AttentionName: "",
-            PhoneNumber: "",
-            Address: {
-                AddressLine: "",
-                City: "",
-                StateProvinceCode: "",
-                PostalCode: "",
-                CountryCode: ""
-            }
-        },
-        PaymentInformation: {
-            ShipmentCharge: {
-                Type: "01",
-                BillShipper: {
-                    AccountNumber: ups.options.ShipperNumber
-                }                
-            }            
-        },        
-        Service: {
-            Code: "11",            
-        },
-        RateInformation: {
-            NegotiatedRatesIndicator: ""
-        },
-        ReferenceNumber: {
-            Code: "02",
-            Value: "my reference id"
-        },        
-        Package: {
-            Description: "Rate",
-            Packaging: {
-                Code: "02",//02=piece, 01=envelope            
-            },            
-            Dimensions: {
-                UnitOfMeasurement: {
-                    Code: "CM",
-                    Description: "centimeter"
-                },
-                Length: "10",
-                Width: "10",
-                Height: "1"
-            },
-            PackageWeight: {
-                UnitOfMeasurement: {
-                    Code: "KGS",
-                    Description: "KILLOGRAMM"
-                },
-                Weight: "1"
-            },
-            PackageServiceOptions: {
-                InsuredValue: {
-                    CurrencyCode: "EUR",
-                    MonetaryValue: 1
-                }
-            }            
-        }       
-    },
-    LabelSpecification: {
-        LabelPrintMethod: {
-            Code: "GIF",
-            Description: "gif file"
-        },
-        HTTPUserAgent: "Mozilla/4.5",
-        LabelStockSize: {
-            Height: 4,
-            Width: 8
-        },
-        LabelImageFormat: {
-            Code: "GIF",
-            Description: "gif file"
-        },
-    },
-    
-    
-    
-  }, function(err, resShipment) {
-    if(err) {
-      return console.log("err:", err.detail.Errors);
-    }
-  
-    console.log(resShipment);
-
-    let {ShipmentDigest} = resShipment.ShipmentResults;
-
-    ups.accept({
-        Request: {      
-            RequestAction: "ShipAccept",
-            TransactionReference: {
-                CustomerContext: "Context"
-            }            
-        },
-        ShipmentDigest: ShipmentDigest
-      }, function(err, res) {
-        if(err) {
-            return console.log("err:", err.detail.Errors);
-        }        
-    })
-});
+ups.track("trackingId").then((response) => {
+    console.log("response", response.trackResponse);
+})
 
 /**
  * Pickup
  * 
  */
-
 /**
  * Pickup Piece Type
  * 
@@ -202,196 +75,240 @@ var ups = new upsAPI({
  * SecurityCode: 3 or 4 digit
  */
 
- ups.pickup({
-    Request: {
-      RequestOption: "",
-      TransactionReference: {
-          CustomerContext: ""
-      }      
-    },
-    RatePickupIndicator: "Y",
-    TaxInformationIndicator: "Y",
-    Shipper: {
-        Account: {
-            AccountNumber: ups.options.ShipperNumber,
-            AccountCountryCode: ""
-        }
-    },
-    PickupDateInfo: {
-        CloseTime: "",
-        ReadyTime: "",
-        PickupDate: ""//YYYYMMDD
-    },
-    PickupAddress: {
-        CompanyName: "",
-        ContactName: "",
-        AddressLine: "",
-        City: "",
-        StateProvince: "",
-        PostalCode: "",
-        CountryCode: "",
-        Phone: {
-            Number: "",            
+ups.pickup({
+        Request: {
+          RequestOption: "",
+          TransactionReference: {
+              CustomerContext: ""
+          }      
         },
-        ResidentialIndicator: "Y"
-    },
-    AlternateAddressIndicator: "Y",
-    PickupPiece: {
-        ServiceCode: "001",
-        Quantity: 1,
-        DestinationCountryCode: "",
-        ContainerCode: "01"
-    },
-    TotalWeight: {
-        Weight: "1.0",
-        UnitOfMeasurement: "KGS"
-    },
-    OverweightIndicator: "N",
-    PaymentMethod: "01"
-    
-  }, function(err, res) {
-    if(err) {
-      return console.log("err:", err);
-    }
-  
-    console.log(res);
-});
-
-/**
- * Tracking
- */
- ups.tracking({
-    Request: {
-      TransactionReference: {
-        CustomerContext: ""
-      },
-      RequestAction: "Track",      
-      RequestOption: "activity",
+        RatePickupIndicator: "Y",
+        TaxInformationIndicator: "Y",
+        Shipper: {
+            Account: {
+                AccountNumber: ups.shipperNumber,
+                AccountCountryCode: "DE"
+            }
+        },
+        PickupDateInfo: {
+            CloseTime: "1400",
+            ReadyTime: "0500",
+            PickupDate: "20240725"//YYYYMMDD
+        },
+        PickupAddress: {
+            CompanyName: "",
+            ContactName: "",
+            AddressLine: "",
+            City: "",
+            StateProvince: "",
+            PostalCode: "",
+            CountryCode: "",
+            Phone: {
+                Number: "",            
+            },
+            ResidentialIndicator: "Y"
+        },
+        AlternateAddressIndicator: "Y",
+        ServiceDateOption: "02",
+        PickupPiece: {
+            ServiceCode: "001",
+            Quantity: 1,
+            DestinationCountryCode: "US",
+            ContainerCode: "01"
+        },
+        TotalWeight: {
+            Weight: "1.0",
+            UnitOfMeasurement: "KGS"
+        },
+        OverweightIndicator: "N",
+        PaymentMethod: "01"
+        
+      }, function(err, res) {
+        if(err) {
+          return console.log("err:", err);
+        }
       
-    },    
-    InquiryNumber: "YOUR SHIPMENT ID here"
-  }, function(err, res) {
-    if(err) {
-      return console.log("err:", err.detail.Errors);
-    }
-  
-    console.log(res);
-}); 
+        console.log(res);
+}).then((response) => {
+    console.log("response", response.PickupRateResponse);
+})
 
 
-
-/**
- * TimeInTransit
- */
- ups.timeintransit({
+ups.rate({
     Request: {
-      RequestOption: "TNT",
-      TransactionReference: {
-          CustomerContext: ""
-      },
-      RequestAction: "TimeInTransit",  
+        TransactionReference: {
+            CustomerContext: ""
+        },  
     },
-    ShipFrom: {
-        Address: {
-            City: "From City here",
-            PostalCode: "From ZipCode here",
-            CountryCode: "From Country here",
-        }
+    PickupType: {
+        Code: "01"//01=package, 19=envelope
     },
-    ShipTo: {
-        Address: {
-            City: "To City here",
-            PostalCode: "To ZipCode here",
-            CountryCode: "From Country here",
-            StateProvinceCode: "",
-        }
-    },
-    Pickup: {
-        Date: "20220222"//YYYYMMDD
-    },    
-    ShipmentWeight: {
-        Weight: "",
-        UnitOfMeasurement: {
-            Code: "KGS",
-            Description: "Killogramm",        
-        }
-    },
-    TotalPackagesInShipment: "",
-    InvoiceLineTotal: {
-        CurrencyCode: "EUR",
-        MonetaryValue: ""
-    },
-    MaximumListSize: 1
-    
-  }, function(err, res) {
-    if(err) {
-      return console.log("err:", err);
-    }
-  
-    console.log(res);
-});
-
-
-/**
- *  Rates
- */
- ups.rates({
-  Request: {
-    RequestOption: "Shop",
-    TransactionReference: {
-        CustomerContext: "Time2ship"
+    CustomerClassification: {
+        Code: "00"
     },  
-  },
-  PickupType: {
-    Code: "01"//01=package, 19=envelope
-  },
-  CustomerClassification: {
-    Code: "00"
-  },
-  Shipment: {
-    Shipper: {
-        ShipperNumber: ups.options.ShipperNumber,
-        Address: {
-            City: "From City here",
-            PostalCode: "From ZipCode here",
-            CountryCode: "From Country here",
-        }
-    },
-    ShipTo: {
-        Address: {
-            City: "To City here",
-            PostalCode: "To ZipCode here",
-            CountryCode: "From Country here",
-            StateProvinceCode: "",
-        }
-    },    
-    Package: {
-        PackagingType: {
-            Code: "02",//02=piece, 01=envelope
-            Description: "Rate"
+    Shipment: {
+        Shipper: {
+            ShipperNumber: ups.shipperNumber,
+            Address: {
+                City: "From City here",
+                PostalCode: "From ZipCode here",
+                CountryCode: "From Country here",
+            }
         },
-        Dimensions: {
-            UnitOfMeasurement: {
-                Code: "CM",
-                Description: "centimeter"
+        ShipTo: {
+            Address: {
+                City: "To City here",
+                PostalCode: "To ZipCode here",
+                CountryCode: "From Country here",
+                StateProvinceCode: "",
+            }
+        }, 
+        Package: {
+            PackagingType: {
+                Code: "02",//02=piece, 01=envelope
+                Description: "Rate"
             },
-            Length: "",
-            Width: "",
-            Height: ""
-        },
-        PackageWeight: {
-            UnitOfMeasurement: {
-                Code: "KGS",
-                Description: "Killogramm"
+            Dimensions: {
+                UnitOfMeasurement: {
+                    Code: "CM",
+                    Description: "centimeter"
+                },
+                Length: "12",
+                Width: "12",
+                Height: "12"
             },
-            Weight: ""
+            PackageWeight: {
+                UnitOfMeasurement: {
+                    Code: "KGS",
+                    Description: "KILLOGRAMM"
+                },
+                Weight: "1"
+            }
         }
     }
-  }
-}, function(err, res) {
-  if(err) {
-    return console.log("err:", err);
-  }
+}).then((data) => {
+    console.log("data", data);
+})
 
-  console.log(res);
-});
+
+ups.ship({
+    Request: {      
+        TransactionReference: {
+            CustomerContext: ""
+        },      
+        RequestAction: "ShipConfirm",
+        RequestOption: "nonvalidate",
+      },    
+      Shipment: {        
+          Description: "My Shipment",
+          Shipper: {
+            Name: "",
+            AttentionName: "",
+            ShipperNumber: ups.options.ShipperNumber,
+            PhoneNumber: "",            
+            
+            Address: {
+                AddressLine: "",                
+                City: "",                
+                PostalCode: "",
+                CountryCode: ""
+            }
+          },
+          ShipTo: {
+            Name: "",
+            AttentionName: "",
+            PhoneNumber: "",
+            Address: {
+                AddressLine: "",
+                City: "",
+                StateProvinceCode: "",
+                PostalCode: "",
+                CountryCode: ""
+            }
+          },
+          PaymentInformation: {
+              ShipmentCharge: {
+                  Type: "01",
+                  BillShipper: {
+                      AccountNumber: ups.shipperNumber
+                  }                
+              }            
+          },        
+          Service: {
+              Code: "11",            
+          },
+          RateInformation: {
+              NegotiatedRatesIndicator: ""
+          },
+          ReferenceNumber: {
+              Code: "02",
+              Value: "my reference id"
+          },        
+          Package: {
+              Description: "Rate",
+              Packaging: {
+                  Code: "02",//02=piece, 01=envelope            
+              },            
+              Dimensions: {
+                  UnitOfMeasurement: {
+                      Code: "CM",
+                      Description: "centimeter"
+                  },
+                  Length: "12",
+                  Width: "12",
+                  Height: "12"
+              },
+              PackageWeight: {
+                  UnitOfMeasurement: {
+                      Code: "KGS",
+                      Description: "KILLOGRAMM"
+                  },
+                  Weight: "1"
+              },
+              PackageServiceOptions: {
+                  InsuredValue: {
+                      CurrencyCode: "EUR",
+                      MonetaryValue: 5
+                  }
+              }            
+          }       
+      },
+      LabelSpecification: {
+          LabelPrintMethod: {
+              Code: "GIF",
+              Description: "gif file"
+          },
+          HTTPUserAgent: "Mozilla/4.5",
+          LabelStockSize: {
+              Height: 4,
+              Width: 8
+          },
+          LabelImageFormat: {
+              Code: "GIF",
+              Description: "gif file"
+          },
+    }
+}).then((response) => {
+    console.log("response", response);
+})
+
+ups.timeInTransit({
+    originCountryCode: 'DE',
+    originPostalCode: 'Zip',
+    originCityName: 'City',
+    destinationCountryCode: 'DE',
+    destinationStateProvince: '',
+    destinationCityName: 'City',
+    destinationPostalCode: 'Zip',
+    weight: 1,
+    weightUnitOfMeasure: 'KGS',
+    shipmentContentsValue: 1,
+    shipmentContentsCurrencyCode: 'EUR',
+    billType: '03',
+    shipDate: '2024-07-24',
+    shipTime: '10:00',
+    avvFlag: true,
+    numberOfPackages: 1
+}).then((response) => {
+    console.log("response", response.emsResponse);
+}); 
